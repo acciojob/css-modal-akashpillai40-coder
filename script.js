3c92d6e0b1934218735b0faf003385a1c9fae60f
Akash <akashpillai40@gmail.com>
2026-03-26 19:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const openBtn = document.getElementById("openModal");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close-modal");
+
+// Open modal
+openBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+// Close modal (X button)
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close when clicking outside
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
